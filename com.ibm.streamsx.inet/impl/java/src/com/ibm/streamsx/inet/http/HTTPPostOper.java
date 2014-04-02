a9cb518bc1f888332d8caf6be736cb20cb52da35
Rohit Wagle <ro.wagle@gmail.com>
2014-04-02 10:22:05 -0400

    more clarification on the property file path #22

--- com.ibm.streamsx.inet/impl/java/src/com/ibm/streamsx/inet/http/HTTPPostOper.java
+++ com.ibm.streamsx.inet/impl/java/src/com/ibm/streamsx/inet/http/HTTPPostOper.java
@@ -65,7 +65,8 @@ public class HTTPPostOper extends AbstractOperator
 	public void setAuthenticationType(String val) {
 		this.authType = val;
 	}
-	@Parameter(optional=true, description="Path to the properties file containing authentication information."+
+	@Parameter(optional=true, description=
+			"Path to the properties file containing authentication information. Path can be absolute or relative to the data directory of the application."+
 			" See http_auth_basic.properties in the toolkits config directory for a sample of basic authentication properties.")
 	public void setAuthenticationFile(String val) {
 		this.authFile = val;
