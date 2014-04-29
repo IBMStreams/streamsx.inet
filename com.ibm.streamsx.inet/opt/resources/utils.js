39e5bf355275379ecb57f147ce47731f64c2fe4b
ddebrunner <djd@debrunners.com>
2014-04-29 09:36:14 -0400

    Add formatter for int64 time attributes.

--- com.ibm.streamsx.inet/opt/resources/utils.js
+++ com.ibm.streamsx.inet/opt/resources/utils.js
@@ -1,5 +1,6 @@
 localTimeString = function(seconds) { return new Date(seconds*1000).toLocaleTimeString(); };
 localTimestampString = function(seconds) { return new Date(seconds*1000).toLocaleString(); };
+localTimeMillisString = function(ms) { return new Date(ms).toLocaleString(); };
 
 fmtLink = function(path) {
   return '<a href="' + path + '">' + path + '</a>';
