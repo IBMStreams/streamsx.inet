26757bd4c34bd51cac4a5fbd0b097b34eca43305
joergboe <joergboe@de.ibm.com>
2016-12-07 15:50:00 +0100

    Common translation units

--- com.ibm.streamsx.inet/impl/java/src/com/ibm/streamsx/inet/messages/Messages.java
+++ com.ibm.streamsx.inet/impl/java/src/com/ibm/streamsx/inet/messages/Messages.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "com.ibm.streamsx.inet.messages.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.ibm.streamsx.inet.messages.messages";
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
