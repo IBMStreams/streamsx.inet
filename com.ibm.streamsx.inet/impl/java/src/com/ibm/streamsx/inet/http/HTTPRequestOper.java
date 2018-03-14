5ee1f50a2c265929b79dcd90a2b47f5fb123487f
joergboe <joergboe@de.ibm.com>
2018-03-14 17:12:25 +0100

    First running example of HTTPRequest operator

--- com.ibm.streamsx.inet/impl/java/src/com/ibm/streamsx/inet/http/HTTPRequestOper.java
+++ com.ibm.streamsx.inet/impl/java/src/com/ibm/streamsx/inet/http/HTTPRequestOper.java
@@ -6,10 +6,15 @@
 //
 package com.ibm.streamsx.inet.http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpResponse;
@@ -34,11 +39,24 @@ import com.ibm.streams.operator.model.InputPortSet;
 import com.ibm.streams.operator.model.InputPorts;
 import com.ibm.streams.operator.model.Libraries;
 import com.ibm.streams.operator.model.PrimitiveOperator;
+import com.ibm.streams.operator.model.OutputPortSet;
+import com.ibm.streams.operator.model.OutputPortSet.WindowPunctuationOutputMode;
+import com.ibm.streams.operator.model.OutputPorts;
+import com.ibm.streams.operator.OutputTuple;
+import com.ibm.streams.operator.StreamingOutput;
 
-@InputPorts(@InputPortSet(cardinality = 1, description = "Each tuple results in an HTTP request."))
 @PrimitiveOperator(name = "HTTPRequest", description = HTTPRequestOperAPI.DESC)
 @Libraries("opt/httpcomponents-client-4.5.5/lib/*")
 @Icons(location32 = "icons/HTTPPost_32.gif", location16 = "icons/HTTPPost_16.gif")
+@InputPorts(@InputPortSet(cardinality = 1, description = "Each tuple results in an HTTP request."))
+@OutputPorts(
+	{
+		@OutputPortSet(
+			cardinality=1, optional=false, windowPunctuationOutputMode=WindowPunctuationOutputMode.Generating,
+			description="Data received from the server will be sent on this port."
+		)
+	}
+)
 public class HTTPRequestOper extends HTTPRequestOperClient {
 
     @Override
@@ -57,6 +75,7 @@ public class HTTPRequestOper extends HTTPRequestOperClient {
             request = createPut(url, tuple);
             break;
         case GET:
+        	System.out.println("Create get");
             request = createGet(url, tuple);
             break;
         case CONNECT:
@@ -111,16 +130,52 @@ public class HTTPRequestOper extends HTTPRequestOperClient {
         req.setEntity(new StringEntity(jo.serialize(), ContentType.APPLICATION_JSON));
     }
 
-    void sendRequest(HttpRequestBase request) throws ClientProtocolException, IOException {
+    void sendRequest(HttpRequestBase request) throws ClientProtocolException, IOException, Exception {
         try {
+        	System.out.println("request="+request.toString());
+        	System.out.println("client="+getClient().toString());
             HttpResponse response = getClient().execute(request);
 
             // TODO error handling
             @SuppressWarnings("unused")
             StatusLine status = response.getStatusLine();
-
+            System.out.println("status="+status.toString());
             HttpEntity entity = response.getEntity();
+            String resp = "";
+            String contentType="";
+            String contentEncoding="";
+            if (entity != null) {
+                Boolean isCh = entity.isChunked();
+                Boolean isRep = entity.isRepeatable();
+                Boolean isStr = entity.isStreaming();
+                System.out.println("entitiy isChunked="+isCh.toString()+ " isRepeatable="+isRep+" isStreaming="+isStr);
+                Header contEnc = entity.getContentEncoding();
+                if (contEnc != null) contentEncoding = contEnc.toString();
+                Header contType = entity.getContentType();
+                if (contentType != null) contentType = contType.toString();
+                InputStream instream = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(instream));
+                String ln = null;
+                while ((ln = reader.readLine()) != null) {
+                    resp = resp + ln + "\n";
+                }
+            }
             EntityUtils.consume(entity);
+            StreamingOutput<OutputTuple> op = getOutput(0);
+            OutputTuple otup = op.newTuple();
+            otup.setString("status", status.toString());
+            otup.setString("contentEncoding", contentEncoding);
+            otup.setString("contentType", contentType);
+            otup.setString("data", resp);
+            String respHeader = "";
+            HeaderIterator hi = response.headerIterator();
+            while (hi.hasNext()) {
+                Header myh = hi.nextHeader();
+                respHeader = respHeader + "||" + myh;
+            }
+            //Header[] ha = response.getAllHeaders();
+            otup.setString("responseHeader", respHeader);
+            op.submit(otup);
         } finally {
             request.reset();
         }
