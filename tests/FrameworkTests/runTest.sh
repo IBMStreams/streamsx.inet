88afa8712514029636843b6c67241cd1955af5e0
joergboe <joergboe@de.ibm.com>
2018-04-25 15:49:07 +0200

    add test HTTPRequestCompile

--- tests/FrameworkTests/runTest.sh
+++ tests/FrameworkTests/runTest.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./scripts/bin/runTTF -i tests "$@"
-
+if [[ -e ./scripts/bin/runTTF ]]; then
+	./scripts/bin/runTTF -i tests "$@"
+else
+	echo "./scripts/bin/runTTF is not existing! Run the script './installTestFramework.sh' first"
+	exit 1
+fi
