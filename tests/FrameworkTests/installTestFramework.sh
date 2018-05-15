47ddcfbd82fcaf2cbd8a5c43d4ac88bdbe79e33a
joergboe <joergboe@de.ibm.com>
2018-05-15 16:01:32 +0200

    Improved testframework execution scripts

--- tests/FrameworkTests/installTestFramework.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./testframeInstaller_v2.6.2__dev__.sh "$PWD/scripts"
--- tests/FrameworkTests/runTest.sh
+++ tests/FrameworkTests/runTest.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+installer=$(echo -n testframeInstaller_*.sh)
+echo "Testframework installer is $installer"
+if [[ ( ! -e scripts ) || ( scripts -ot $installer ) ]]; then
+	if ! eval ./$installer "$PWD/scripts"; then
+		echo "Error during testframework installation" >&2
+		exit 1
+	fi
+fi
 if [[ -e ./scripts/bin/runTTF ]]; then
 	./scripts/bin/runTTF -i tests "$@"
 else
-	echo "./scripts/bin/runTTF is not existing! Run the script './installTestFramework.sh' first"
+	echo "./scripts/bin/runTTF is not existing! Run the script './installTestFramework.sh' first" >&2
 	exit 1
 fi
