84f4deced93f26b9d59bfb08afd22015e6a3efa3
joergboe <joergboe@de.ibm.com>
2018-09-18 13:02:27 +0200

    HTTP test server add redirect-to servlet and adapt test case

--- tests/FrameworkTests/HTTPTestServer/src/com/ibm/streamsx/inet/test/httptestserver/HTTPTestServer.java
+++ tests/FrameworkTests/HTTPTestServer/src/com/ibm/streamsx/inet/test/httptestserver/HTTPTestServer.java
@@ -29,72 +29,73 @@ public class HTTPTestServer {
 		int httpPort = 8097;
 		int httpsPort = 1443;
 		
-        Server server = new Server();
-     
-        HttpConfiguration http_config = new HttpConfiguration();
-        http_config.setSecureScheme("https");
-        http_config.setSecurePort(httpsPort);
-        http_config.setOutputBufferSize(32768);
+		Server server = new Server();
 
-        // HTTP connector
-        ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config));
-        //http.setHost("localhost");
-        http.setPort(httpPort);
-        http.setIdleTimeout(30000);
-        
-        // ssl context
-        SslContextFactory sslContextFactory = new SslContextFactory();
-        //sslContextFactory.setKeyStorePath("/home/joergboe/git2/toolkit.streamsx.inet/tests/com.ibm.streams.inet.junit/resources/keystore");
-        sslContextFactory.setKeyStorePath("etc/keystore");
-        //sslContextFactory.setKeyStorePassword("");
-        sslContextFactory.setKeyManagerPassword("test");
-        //sslContextFactory.setKeyStorePath("/home/joergboe/jetty/jetty-distribution-9.4.12.v20180830/demo-base/etc/keystore");
-        //sslContextFactory.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
-        //sslContextFactory.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
-        System.out.println(sslContextFactory.dump());
-        
-        // HTTPS Configuration
-        // A new HttpConfiguration object is needed for the next connector and
-        // you can pass the old one as an argument to effectively clone the
-        // contents. On this HttpConfiguration object we add a
-        // SecureRequestCustomizer which is how a new connector is able to
-        // resolve the https connection before handing control over to the Jetty
-        // Server.
-        HttpConfiguration https_config = new HttpConfiguration(http_config);
-        SecureRequestCustomizer src = new SecureRequestCustomizer();
-        src.setStsMaxAge(2000);
-        src.setStsIncludeSubDomains(true);
-        https_config.addCustomizer(src);
-        
-        // HTTPS connector
-        // We create a second ServerConnector, passing in the http configuration
-        // we just made along with the previously created ssl context factory.
-        // Next we set the port and a longer idle timeout.
-        ServerConnector https = new ServerConnector(server,
-        		new SslConnectionFactory(sslContextFactory,HttpVersion.HTTP_1_1.asString()),
-        		new HttpConnectionFactory(https_config));
-        https.setPort(httpsPort);
-        https.setIdleTimeout(500000);
-        
-        server.setConnectors(new Connector[] { http, https });
+		HttpConfiguration http_config = new HttpConfiguration();
+		http_config.setSecureScheme("https");
+		http_config.setSecurePort(httpsPort);
+		http_config.setOutputBufferSize(32768);
 
-        ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
+		// HTTP connector
+		ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config));
+		//http.setHost("localhost");
+		http.setPort(httpPort);
+		http.setIdleTimeout(30000);
 
-        handler.addServletWithMapping(HelloServlet.class, "/hello/*");
-        handler.addServletWithMapping(HeaderServlet.class, "/headers");
-        handler.addServletWithMapping(MethodServlet.class, "/get");
-        handler.addServletWithMapping(MethodServlet.class, "/delete");
-        handler.addServletWithMapping(MethodServlet.class, "/patch");
-        handler.addServletWithMapping(MethodServlet.class, "/post");
-        handler.addServletWithMapping(MethodServlet.class, "/put");
-        handler.addServletWithMapping(MethodServlet.class, "/head");
-        handler.addServletWithMapping(RedirectServlet.class, "/redirect/*");
-        handler.addServletWithMapping(StatusServlet.class, "/status/*");
+		// ssl context
+		SslContextFactory sslContextFactory = new SslContextFactory();
+		//sslContextFactory.setKeyStorePath("/home/joergboe/git2/toolkit.streamsx.inet/tests/com.ibm.streams.inet.junit/resources/keystore");
+		sslContextFactory.setKeyStorePath("etc/keystore");
+		//sslContextFactory.setKeyStorePassword("");
+		sslContextFactory.setKeyManagerPassword("test");
+		//sslContextFactory.setKeyStorePath("/home/joergboe/jetty/jetty-distribution-9.4.12.v20180830/demo-base/etc/keystore");
+		//sslContextFactory.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
+		//sslContextFactory.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
+		System.out.println(sslContextFactory.dump());
 
-        server.start();
-        server.dumpStdErr();
-        server.join();
+		// HTTPS Configuration
+		// A new HttpConfiguration object is needed for the next connector and
+		// you can pass the old one as an argument to effectively clone the
+		// contents. On this HttpConfiguration object we add a
+		// SecureRequestCustomizer which is how a new connector is able to
+		// resolve the https connection before handing control over to the Jetty
+		// Server.
+		HttpConfiguration https_config = new HttpConfiguration(http_config);
+		SecureRequestCustomizer src = new SecureRequestCustomizer();
+		src.setStsMaxAge(2000);
+		src.setStsIncludeSubDomains(true);
+		https_config.addCustomizer(src);
+		
+		// HTTPS connector
+		// We create a second ServerConnector, passing in the http configuration
+		// we just made along with the previously created ssl context factory.
+		// Next we set the port and a longer idle timeout.
+		ServerConnector https = new ServerConnector(server,
+				new SslConnectionFactory(sslContextFactory,HttpVersion.HTTP_1_1.asString()),
+				new HttpConnectionFactory(https_config));
+		https.setPort(httpsPort);
+		https.setIdleTimeout(500000);
+		
+		server.setConnectors(new Connector[] { http, https });
+
+		ServletHandler handler = new ServletHandler();
+		server.setHandler(handler);
+
+		handler.addServletWithMapping(HelloServlet.class, "/hello/*");
+		handler.addServletWithMapping(HeaderServlet.class, "/headers");
+		handler.addServletWithMapping(MethodServlet.class, "/get");
+		handler.addServletWithMapping(MethodServlet.class, "/delete");
+		handler.addServletWithMapping(MethodServlet.class, "/patch");
+		handler.addServletWithMapping(MethodServlet.class, "/post");
+		handler.addServletWithMapping(MethodServlet.class, "/put");
+		handler.addServletWithMapping(MethodServlet.class, "/head");
+		handler.addServletWithMapping(RedirectServlet.class, "/redirect/*");
+		handler.addServletWithMapping(StatusServlet.class, "/status/*");
+		handler.addServletWithMapping(RedirectToServlet.class, "/redirect-to");
+
+		server.start();
+		server.dumpStdErr();
+		server.join();
 
 	}
 
--- tests/FrameworkTests/tests/CollectionStreamsxInet/Runtests/HTTPRequestRedirect/TestCase.sh
+++ tests/FrameworkTests/tests/CollectionStreamsxInet/Runtests/HTTPRequestRedirect/TestCase.sh
@@ -2,18 +2,18 @@
 
 function myExplain {
 	case "$TTRO_variantCase" in
-	0)  echo "variant $TTRO_variantCase - GET  method url http://httpbin.org/redirect-to?url=http://httpbin.org/get&status_code=307";;
-	1)  echo "variant $TTRO_variantCase - HEAD method url http://httpbin.org/redirect-to?url=http://httpbin.org/get&status_code=307";;
-	2)  echo "variant $TTRO_variantCase - POST method url http://httpbin.org/redirect-to?url=http://httpbin.org/post&status_code=307";;
-	3)  echo "variant $TTRO_variantCase - PUT  method url http://httpbin.org/redirect-to?url=http://httpbin.org/put&status_code=307";;
-	4)  echo "variant $TTRO_variantCase - GET  method url http://httpbin.org/redirect-to?url=http://httpbin.org/get&status_code=307  RELAXED";;
-	5)  echo "variant $TTRO_variantCase - HEAD method url http://httpbin.org/redirect-to?url=http://httpbin.org/get&status_code=307  RELAXED";;
-	6)  echo "variant $TTRO_variantCase - POST method url http://httpbin.org/redirect-to?url=http://httpbin.org/post&status_code=307 RELAXED";;
-	7)  echo "variant $TTRO_variantCase - PUT  method url http://httpbin.org/redirect-to?url=http://httpbin.org/put&status_code=307  RELAXED";;
-	8)  echo "variant $TTRO_variantCase - GET  method url http://httpbin.org/redirect-to?url=http://httpbin.org/get&status_code=307  NONE";;
-	9)  echo "variant $TTRO_variantCase - HEAD method url http://httpbin.org/redirect-to?url=http://httpbin.org/get&status_code=307  NONE";;
-	10) echo "variant $TTRO_variantCase - POST method url http://httpbin.org/redirect-to?url=http://httpbin.org/post&status_code=307 NONE";;
-	11) echo "variant $TTRO_variantCase - PUT  method url http://httpbin.org/redirect-to?url=http://httpbin.org/put&status_code=307  NONE";;
+	0)  echo "variant $TTRO_variantCase - GET  method DEFAULT redirectStrategy : yes url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/get&status_code=307";;
+	1)  echo "variant $TTRO_variantCase - HEAD method DEFAULT redirectStrategy : yes url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/head&status_code=307";;
+	2)  echo "variant $TTRO_variantCase - POST method DEFAULT redirectStrategy : no  url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/post&status_code=307";;
+	3)  echo "variant $TTRO_variantCase - PUT  method DEFAULT redirectStrategy : no  url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/put&status_code=307";;
+	4)  echo "variant $TTRO_variantCase - GET  method RELAXED redirectStrategy : yes url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/get&status_code=307";;
+	5)  echo "variant $TTRO_variantCase - HEAD method RELAXED redirectStrategy : yes url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/head&status_code=307";;
+	6)  echo "variant $TTRO_variantCase - POST method RELAXED redirectStrategy : yes url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/post&status_code=307";;
+	7)  echo "variant $TTRO_variantCase - PUT  method RELAXED redirectStrategy : no  url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/put&status_code=307";;
+	8)  echo "variant $TTRO_variantCase - GET  method NONE redirectStrategy    : no  url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/get&status_code=307";;
+	9)  echo "variant $TTRO_variantCase - HEAD method NONE redirectStrategy    : no  url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/head&status_code=307";;
+	10) echo "variant $TTRO_variantCase - POST method NONE redirectStrategy    : no  url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/post&status_code=307";;
+	11) echo "variant $TTRO_variantCase - PUT  method NONE redirectStrategy    : no  url http://$TTPR_httpServerAddr/redirect-to?url=http://$TTPR_httpServerAddr/put&status_code=307";;
 	*) printErrorAndExit "invalid variant $TTRO_variantCase" $errRt;;
 	esac
 }
@@ -21,7 +21,7 @@ function myExplain {
 PREPS='myExplain copyAndMorphSpl'
 
 STEPS=(
-	'splCompile'
+	"splCompile host=$TTPR_httpServerAddr"
 	'submitJob'
 	'checkJobNo'
 	'waitForFinAndHealth'
@@ -34,11 +34,11 @@ FINS='cancelJob'
 function myEval {
 	case "$TTRO_variantCase" in
 	0|4|6)
-		linewisePatternMatchInterceptAndSuccess "$TT_dataDir/Tuples" "true" "*id=0*" "*stat=200*" '*Host\\": \\"httpbin.org*' '*err=""*';;
+		linewisePatternMatchInterceptAndSuccess "$TT_dataDir/Tuples" "true" "*id=0*" "*stat=200*" "*Host: $TTPR_httpServerAddr*" '*err=""*';;
 	1|5)
 		linewisePatternMatchInterceptAndSuccess "$TT_dataDir/Tuples" "true" "*id=0*" "*stat=200*" '*respData=""*' '*err=""*';;
 	2|3|7|8|9|10|11)
-		linewisePatternMatchInterceptAndSuccess "$TT_dataDir/Tuples" "true" "*id=0*" "*stat=307*" '*respData=""*' '*err=""*';;
+		linewisePatternMatchInterceptAndSuccess "$TT_dataDir/Tuples" "true" "*id=0*" "*stat=307*" '*err=""*';;
 	esac
 }
 
