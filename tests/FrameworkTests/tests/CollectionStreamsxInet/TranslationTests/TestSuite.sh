abacbc32616a465556aba5c46c181482d19951eb
joergboe <joergboe@de.ibm.com>
2020-02-13 13:32:10 +0100

    Remoce languages ru and ko from testlist

--- tests/FrameworkTests/tests/CollectionStreamsxInet/TranslationTests/TestSuite.sh
+++ tests/FrameworkTests/tests/CollectionStreamsxInet/TranslationTests/TestSuite.sh
@@ -1,4 +1,4 @@
-#--variantList='de_DE fr_FR it_IT es_ES pt_BR ja_JP zh_CN ru_RU zh_TW ko_KR en_US'
+#--variantList='de_DE fr_FR it_IT es_ES pt_BR ja_JP zh_CN zh_TW en_US'
 ##--variantList='en_US'
 
 function testPreparation {
